2527e02c6338daf3dc77eeb6fa23c1bc00971ab8
diogo-vf <diogo.vieira-ferreira@proton.me>
2025-11-20 16:13:14 +0100

    config project

--- svelte.config.js
+++ svelte.config.js
@@ -1,12 +1,26 @@
 import adapter from '@sveltejs/adapter-node';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import path from 'path';
+import { readFileSync } from 'node:fs';
+
+const json = readFileSync(path.resolve('./package.json'), 'utf8');
+const pkg = JSON.parse(json);
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://svelte.dev/docs/kit/integrations
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
-	kit: { adapter: adapter() }
+  // Consult https://svelte.dev/docs/kit/integrations
+  // for more information about preprocessors
+  preprocess: vitePreprocess(),
+  kit: {
+    version: {
+      name: `Presskit v${pkg.version}`
+    },
+    adapter: adapter(),
+    alias: {
+      $enums: path.resolve('./src/lib/enums'),
+      $types: path.resolve('./src/lib/types')
+    }
+  }
 };
 
 export default config;
