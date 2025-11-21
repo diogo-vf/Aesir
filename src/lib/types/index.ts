995a711d9fa5ceab0e22ce80f7695e452bc0f6e3
diogo-vf <vieira-ferreira@lausanne-tourisme.ch>
2025-11-21 16:28:38 +0100

    update types

--- src/lib/types/index.ts
+++ src/lib/types/index.ts
@@ -1,24 +1,65 @@
-import type { ItemType, StatisticCategory }  from '$enums';
+import type { ItemType, StatisticCategory } from '$enums';
 
 type Character = {
   name: string;
   age: number;
-  statistics: Statistic[];
-  inventory: unknown
+  statistics: {
+    maxBase: number;
+    base: number;
+    actual: number;
+    statistic: Statistic;
+  }[];
+  inventory: Inventory;
+  equipment: Equipment;
+  skills: Skill[];
+  divineBlood: DivineBlood,
+  divineBlessing: DivineBlessing,
 };
 
 type Statistic = {
+  id: number;
   name: string;
   description: string;
   category: StatisticCategory;
-  base: number;
-  actual: number;
-  max_base: number;
 };
 
 type Item = {
+  id: number;
+  name: string;
+  description: string;
+  stackable: boolean;
+  type: ItemType;
+};
+
+type Inventory = {
+  maxItems: number;
+  totalItems: number;
+  items: { item: Item; quantity: number }[];
+};
+
+type Equipment = {
+  Helmet?: Item;
+  Chest?: Item;
+  Legs?: Item;
+  Gloves?: Item;
+  RightHand?: Item;
+  LeftHand?: Item;
+  Accessories?: Item[];
+};
+
+type Skill = {
+  id: number;
+  name: string;
+};
+
+type DivineBlood = {
+  id: number,
   name: string,
   description: string,
-  stackable: boolean,
-  type: ItemType,
+}
+
+type DivineBlessing = {
+  id: number,
+  name: string,
+  description: string,
 }
